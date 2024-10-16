a54053d7c79f707da45543d9322c56fed3d52995
Chintan <mangukiyachintan2021@gmail.com>
2024-10-16 18:49:01 +0530

    Last commit

--- src/app/add-edit/add-edit.component.ts
+++ src/app/add-edit/add-edit.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+
+interface std{
+  value: number,
+  viewValue: string
+}
 
 @Component({
   selector: 'app-add-edit',
@@ -10,26 +15,27 @@ export class AddEditComponent implements OnInit{
 
   studentForm : FormGroup;
 
-  classes = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '10',
-    '11 A',
-    '11 B',
-    '12 A',
-    '12 B'
+  standards : std[] = [
+    {value: 1, viewValue: '1'},
+    {value: 2, viewValue: '2'},    
+    {value: 3, viewValue: '3'},
+    {value: 4, viewValue: '4'},
+    {value: 5, viewValue: '5'},
+    {value: 6, viewValue: '6'},
+    {value: 7, viewValue: '7'},
+    {value: 8, viewValue: '8'},
+    {value: 9, viewValue: '9'},
+    {value: 10, viewValue: '10'},
+    {value: 11, viewValue: '11 A'},
+    {value: 12, viewValue: '11 B'},
+    {value: 13, viewValue: '12 A'},
+    {value: 14, viewValue: '12 B'}
   ];
 
   constructor(){
     this.studentForm = new FormGroup({
-      'name': new FormControl(null, [Validators.required])
+      'name': new FormControl('', [Validators.required]),
+      'std' : new FormControl('', [Validators.required])
     })
   }
 
